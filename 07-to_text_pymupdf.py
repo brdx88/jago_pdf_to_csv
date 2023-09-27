35c405826a7ea253662f228bb1651887cdb2d4c6
Brian IC <brian.ic@outlook.com>
2023-09-27 22:53:53 +0700

    successfully fetch the result

--- 07-to_text_pymupdf.py
+++ 07-to_text_pymupdf.py
@@ -1,21 +1,11 @@
-import fitz
-import chardet
+import PyPDF2
 
 pdf_file_path = r"C:\Users\brian\python_automation\Jago_Main Pocket_History_27082023.pdf"
-pdf_document = fitz.open(pdf_file_path)
 
-text = ""
-
-for page_number in range(pdf_document.page_count):
-    page = pdf_document.load_page(page_number)
-    text += page.get_text()
-
-# Detect the encoding using chardet
-detected_encoding = chardet.detect(text.encode())['encoding']
-
-# Decode the text using the detected encoding
-text = text.encode(detected_encoding, 'replace').decode('utf-8', 'replace')
-
-pdf_document.close()
+with open(pdf_file_path, 'rb') as pdf_file:
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
 
 print(text)
