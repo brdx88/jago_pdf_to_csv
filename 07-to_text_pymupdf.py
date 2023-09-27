9b90730cb426624864f924957e5b0b6c923953da
Brian IC <brian.ic@outlook.com>
2023-09-27 23:42:22 +0700

    removing the title docs

--- 07-to_text_pymupdf.py
+++ 07-to_text_pymupdf.py
@@ -2,15 +2,25 @@ import PyPDF2
 
 pdf_file_path = r"C:\Users\brian\python_automation\Jago_Main Pocket_History_27082023.pdf"
 output_file_path = "transaction_output_pypdf2.txt"  # Specify the output file path
+start_line_number = 20  # Specify the line number to start capturing
 
 with open(pdf_file_path, 'rb') as pdf_file:
     pdf_reader = PyPDF2.PdfReader(pdf_file)
-    text = ""
+    transaction_data = []
+    current_line_number = 0
+
     for page in pdf_reader.pages:
-        text += page.extract_text()
+        page_text = page.extract_text()
+        lines = page_text.split('\n')
 
-# Save the extracted text to a text file
+        for line in lines:
+            current_line_number += 1
+            if current_line_number >= start_line_number:
+                transaction_data.append(line)
+
+# Save the extracted transaction data to a text file
 with open(output_file_path, 'w', encoding='utf-8') as output_file:
-    output_file.write(text)
+    for line in transaction_data:
+        output_file.write(line + '\n')
 
-print(f"Text extracted from PDF and saved to {output_file_path}")
+print(f"Transaction data extracted from PDF and saved to {output_file_path}")
