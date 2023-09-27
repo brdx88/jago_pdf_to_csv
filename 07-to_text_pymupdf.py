bd7c8c57e88b71da5daf74fe1a17d0df8e354c6e
Brian IC <brian.ic@outlook.com>
2023-09-27 23:04:20 +0700

    add output script

--- 07-to_text_pymupdf.py
+++ 07-to_text_pymupdf.py
@@ -1,6 +1,7 @@
 import PyPDF2
 
 pdf_file_path = r"C:\Users\brian\python_automation\Jago_Main Pocket_History_27082023.pdf"
+output_file_path = "transaction_output_pypdf2.txt"  # Specify the output file path
 
 with open(pdf_file_path, 'rb') as pdf_file:
     pdf_reader = PyPDF2.PdfReader(pdf_file)
@@ -8,4 +9,8 @@ with open(pdf_file_path, 'rb') as pdf_file:
     for page in pdf_reader.pages:
         text += page.extract_text()
 
-print(text)
+# Save the extracted text to a text file
+with open(output_file_path, 'w', encoding='utf-8') as output_file:
+    output_file.write(text)
+
+print(f"Text extracted from PDF and saved to {output_file_path}")
